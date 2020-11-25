8f98ff57f56c16b225190fee9af8742d626fc42a
Jiangyun Wang <jurainyun@gmail.com>
2020-11-25 00:46:45 -0500

    three 3D models

--- public/Sphere.js
+++ public/Sphere.js
@@ -1,160 +1,122 @@
-// // WebGL - Multiple Objects - List
-// // from https://webglfundamentals.org/webgl/webgl-multiple-objects-list.html
-//
-//
-// "use strict";
-//
-// function main() {
-//
-//     var canvas = document.querySelector("#canvas");
-//     var gl = canvas.getContext("webgl")
-//
-//     // creates buffers with position, normal, texcoord, and vertex color
-//     // data for primitives by calling gl.createBuffer, gl.bindBuffer,
-//     // and gl.bufferData
-//     const sphereBufferInfo = primitives.createSphereWithVertexColorsBufferInfo(gl, 10, 12, 6);
-//     const sphereBufferInfo2 = primitives.createSphereWithVertexColorsBufferInfo(gl, 20, 24, 12);
-//     const cubeBufferInfo   = primitives.createCubeWithVertexColorsBufferInfo(gl, 20);
-//     // const coneBufferInfo   = primitives.createTruncatedConeWithVertexColorsBufferInfo(gl, 10, 0, 20, 12, 1, true, false);
-//
-//     // setup GLSL program
-//     var programInfo = webglUtils.createProgramInfo(gl, ["vertex-shader-3d", "fragment-shader-3d"]);
-//
-//     function degToRad(d) {
-//         return d * Math.PI / 180;
-//     }
-//
-//     var cameraAngleRadians = degToRad(0);
-//     var fieldOfViewRadians = degToRad(60);
-//     var cameraHeight = 50;
-//
-//     // Uniforms for each object.
-//     var sphereUniforms = {
-//         u_colorMult: [0.5, 1, 0.5, 1],
-//         u_matrix: m4.identity(),
-//     };
-//     var cubeUniforms = {
-//         u_colorMult: [1, 0.5, 0.5, 1],
-//         u_matrix: m4.identity(),
-//     };
-//     var coneUniforms = {
-//         u_colorMult: [0.5, 0.5, 1, 1],
-//         u_matrix: m4.identity(),
-//     };
-//     var sphereTranslation = [  0, 0, 0];
-//     var cubeTranslation   = [-40, 0, 0];
-//     var coneTranslation   = [ 40, 0, 0];
-//
-//     var objectsToDraw = [
-//         {
-//             programInfo: programInfo,
-//             bufferInfo: sphereBufferInfo,
-//             uniforms: sphereUniforms,
-//         },
-//         // {
-//         //     programInfo: programInfo,
-//         //     bufferInfo: cubeBufferInfo,
-//         //     uniforms: cubeUniforms,
-//         // },
-//         // {
-//         //     programInfo: programInfo,
-//         //     bufferInfo: coneBufferInfo,
-//         //     uniforms: coneUniforms,
-//         // },
-//         {
-//             programInfo: programInfo,
-//             bufferInfo: sphereBufferInfo2,
-//             uniforms: coneUniforms,
-//         },
-//     ];
-//
-//     function computeMatrix(viewProjectionMatrix, translation, xRotation, yRotation) {
-//         var matrix = m4.translate(viewProjectionMatrix,
-//             translation[0],
-//             translation[1],
-//             translation[2]);
-//         matrix = m4.xRotate(matrix, xRotation);
-//         return m4.yRotate(matrix, yRotation);
-//     }
-//
-//     requestAnimationFrame(drawScene);
-//
-//     // Draw the scene.
-//     function drawScene(time) {
-//         time *= 0.0005;
-//
-//         webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-//
-//         // Tell WebGL how to convert from clip space to pixels
-//         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-//
-//         gl.enable(gl.CULL_FACE);
-//         gl.enable(gl.DEPTH_TEST);
-//
-//         // Clear the canvas AND the depth buffer.
-//         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-//
-//         // Compute the projection matrix
-//         var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-//         var projectionMatrix =
-//             m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
-//
-//         // Compute the camera's matrix using look at.
-//         var cameraPosition = [0, 0, 100];
-//         var target = [0, 0, 0];
-//         var up = [0, 1, 0];
-//         var cameraMatrix = m4.lookAt(cameraPosition, target, up);
-//
-//         // Make a view matrix from the camera matrix.
-//         var viewMatrix = m4.inverse(cameraMatrix);
-//
-//         var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
-//
-//         var sphereXRotation =  time;
-//         var sphereYRotation =  time;
-//         var cubeXRotation   = -time;
-//         var cubeYRotation   =  time;
-//         var coneXRotation   =  time;
-//         var coneYRotation   = -time;
-//
-//         // Compute the matrices for each object.
-//         sphereUniforms.u_matrix = computeMatrix(
-//             viewProjectionMatrix,
-//             sphereTranslation,
-//             sphereXRotation,
-//             sphereYRotation);
-//
-//         cubeUniforms.u_matrix = computeMatrix(
-//             viewProjectionMatrix,
-//             cubeTranslation,
-//             cubeXRotation,
-//             cubeYRotation);
-//
-//         coneUniforms.u_matrix = computeMatrix(
-//             viewProjectionMatrix,
-//             coneTranslation,
-//             coneXRotation,
-//             coneYRotation);
-//
-//         // ------ Draw the objects --------
-//
-//         objectsToDraw.forEach(function(object) {
-//             var programInfo = object.programInfo;
-//             var bufferInfo = object.bufferInfo;
-//
-//             gl.useProgram(programInfo.program);
-//
-//             // Setup all the needed attributes.
-//             webglUtils.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-//
-//             // Set the uniforms.
-//             webglUtils.setUniforms(programInfo, object.uniforms);
-//
-//             // Draw
-//             gl.drawArrays(gl.TRIANGLES, 0, bufferInfo.numElements);
-//         });
-//
-//         requestAnimationFrame(drawScene);
-//     }
-// }
-//
+function main(THREE) {
+    const canvas = document.querySelector('#canvas');
+    const renderer = new THREE.WebGLRenderer({canvas});
+
+    const fov = 40;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 1000;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 120;
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xAAAAAA);
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+    }
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(1, -2, -4);
+        scene.add(light);
+    }
+
+    const objects = [];
+    const spread = 15;
+
+    function addObject(x, y, obj) {
+        obj.position.x = x * spread;
+        obj.position.y = y * spread;
+
+        scene.add(obj);
+        objects.push(obj);
+    }
+
+    function createMaterial() {
+        const material = new THREE.MeshPhongMaterial({
+            side: THREE.DoubleSide,
+        });
+
+        const hue = Math.random();
+        const saturation = 1;
+        const luminance = .5;
+        material.color.setHSL(hue, saturation, luminance);
+
+        return material;
+    }
+
+    function addSolidGeometry(x, y, geometry) {
+        const mesh = new THREE.Mesh(geometry, createMaterial());
+        addObject(x, y, mesh);
+    }
+
+    function addLineGeometry(x, y, geometry) {
+        const material = new THREE.LineBasicMaterial({color: 0x000000});
+        const mesh = new THREE.LineSegments(geometry, material);
+        addObject(x, y, mesh);
+    }
+
+    {
+        const innerRadius = 60;
+        const outerRadius = 62;
+        const segments = 180;
+        addSolidGeometry(0, 0, new THREE.RingBufferGeometry(innerRadius, outerRadius, segments));
+    }
+
+    {
+        const radius = 30;
+        const widthSegments = 120;
+        const heightSegments = 80;
+        addSolidGeometry(-1, 0, new THREE.SphereBufferGeometry(radius, widthSegments, heightSegments));
+    }
+
+    {
+        const radius = 15;
+        const widthSegments = 60;
+        const heightSegments = 40;
+        addSolidGeometry(4, 0, new THREE.SphereBufferGeometry(radius, widthSegments, heightSegments));
+    }
+
+
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+    function render(time) {
+        time *= 0.001;
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+
+        objects.forEach((obj, ndx) => {
+            const speed = .1 + ndx * .05;
+            const rot = time * speed;
+            obj.rotation.x = rot;
+            obj.rotation.y = rot;
+        });
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+}
+
+// main();
--- public/render.js
+++
@@ -1,8 +0,0 @@
-function render(time) {
-    time *= 0.001;  // convert time to seconds
-    cube.rotation.x = time;
-    cube.rotation.y = time;
-    renderer.render(scene, camera);
-    requestAnimationFrame(render);
-}
-requestAnimationFrame(render);
