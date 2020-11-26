2c6ae6f859375c46ded487e8dbf5676d97c70938
Jiangyun Wang <jurainyun@gmail.com>
2020-11-26 07:34:10 -0500

    adjuested

--- public/main.js
+++ public/main.js
@@ -10,6 +10,55 @@ function main() {
     const aspect = 2;  // the canvas default
     const near = 0.1;
     const far = 1000;
+<<<<<<< HEAD
+=======
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 120;
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xAAAAAA);
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+    }
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(1, -2, -4);
+        scene.add(light);
+    }
+
+    const objects = [];
+    const spread = 15;
+
+
+
+    {
+        const innerRadius = 60;   //---- orbit
+        const outerRadius = 62;
+        const segments = 180;
+        addSolidGeometry(0, 0, new THREE.RingBufferGeometry(innerRadius, outerRadius, segments),THREE,spread,scene,objects);
+    }
+
+    {
+        const radius = 30;
+        const widthSegments = 60;     //---- Sun
+        const heightSegments = 60;
+        addSolidGeometry(-2, 0, new THREE.SphereBufferGeometry(radius, widthSegments, heightSegments),THREE,spread,scene,objects);
+    }
+
+    {
+        const radius = 15;
+        const widthSegments = 60;    //  ---- Earth
+        const heightSegments = 60;
+        addSolidGeometry(4, 0, new THREE.SphereBufferGeometry(radius, widthSegments, heightSegments),THREE,spread,scene,objects);
+    }
+>>>>>>> adjuested
 
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.set(0, 0,200);
