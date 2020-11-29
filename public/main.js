2edf7f653bb6460a07b170ac9ecf21863624d5fd
Jiangyun Wang <jurainyun@gmail.com>
2020-11-28 23:29:28 -0500

    add ackground

--- public/main.js
+++ public/main.js
@@ -3,62 +3,16 @@ import * as THREE from './js/three.module.js';
 
 function main() {
     const canvas = document.querySelector('#canvas');
-    const renderer = new THREE.WebGLRenderer({canvas});
+    const renderer = new THREE.WebGLRenderer({
+        canvas,
+        alpha: true,
+    });
     const scene = new THREE.Scene();
 
     const fov = 40;
     const aspect = 2;  // the canvas default
     const near = 0.1;
     const far = 1000;
-<<<<<<< HEAD
-=======
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 120;
-
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xAAAAAA);
-
-    {
-        const color = 0xFFFFFF;
-        const intensity = 1;
-        const light = new THREE.DirectionalLight(color, intensity);
-        light.position.set(-1, 2, 4);
-        scene.add(light);
-    }
-    {
-        const color = 0xFFFFFF;
-        const intensity = 1;
-        const light = new THREE.DirectionalLight(color, intensity);
-        light.position.set(1, -2, -4);
-        scene.add(light);
-    }
-
-    const objects = [];
-    const spread = 15;
-
-
-
-    {
-        const innerRadius = 60;   //---- orbit
-        const outerRadius = 62;
-        const segments = 180;
-        addSolidGeometry(0, 0, new THREE.RingBufferGeometry(innerRadius, outerRadius, segments),THREE,spread,scene,objects);
-    }
-
-    {
-        const radius = 30;
-        const widthSegments = 60;     //---- Sun
-        const heightSegments = 60;
-        addSolidGeometry(-2, 0, new THREE.SphereBufferGeometry(radius, widthSegments, heightSegments),THREE,spread,scene,objects);
-    }
-
-    {
-        const radius = 15;
-        const widthSegments = 60;    //  ---- Earth
-        const heightSegments = 60;
-        addSolidGeometry(4, 0, new THREE.SphereBufferGeometry(radius, widthSegments, heightSegments),THREE,spread,scene,objects);
-    }
->>>>>>> adjuested
 
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.set(0, 0,200);
@@ -128,5 +82,4 @@ function rotateEarth(obj, time) {
 }
 
 main();
-
 
