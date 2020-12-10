c07f542fc65e24b410cf2ceba17993cbbcb6dfb0
Jiangyun Wang <jurainyun@gmail.com>
2020-12-10 09:56:14 -0500

    add buttons and progress bar in the half way

--- public/main.js
+++ public/main.js
@@ -2,7 +2,41 @@ import {OrbitControls} from './js/examples/OrbitControls.js';
 import * as THREE from './js/three.module.js';
 
 function main() {
+    let start;
+    let pauseTime;
+    let requestID;
+    let state;
+    let currentTime;
+    let speed = 1;
+    const objects = [];
     const canvas = document.querySelector('#canvas');
+
+    document.getElementById('btnResume').addEventListener('click', function (e) {
+        e.preventDefault();
+        state = true
+        requestID = requestAnimationFrame(render);
+    })
+
+   document.getElementById('btnPause').addEventListener('click', function (e) {
+        e.preventDefault();
+        state = false
+        currentTime = pauseTime
+        console.log("pause button: state ", state," currentTime: ",currentTime)
+        cancelAnimationFrame(requestID);
+    });
+
+    document.getElementById('btnSpeedUp').addEventListener('click', function (e) {
+        e.preventDefault();
+        // currentTime = pauseTime
+        speed = speed * 0.75
+    });
+
+    document.getElementById('btnSlowDown').addEventListener('click', function (e) {
+        e.preventDefault();
+        // currentTime = pauseTime
+        speed = speed * 1.25
+    });
+
     const renderer = new THREE.WebGLRenderer({
         canvas,
         alpha: true,
@@ -15,7 +49,7 @@ function main() {
     const far = 1000;
 
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 0,200);
+    camera.position.set(0, 0, 200);
 
     // texture loader to load images onto geometries
     const loader = new THREE.TextureLoader();
@@ -24,7 +58,7 @@ function main() {
     controls.target.set(0, 5, 0)
     controls.update();
 
-    const objects = [];
+
 
     // point light
     const pointlight = new THREE.PointLight('#ffdcb4', 1.5);
@@ -52,28 +86,38 @@ function main() {
 
     // earth geometry
     const earthGeo = new THREE.SphereBufferGeometry(10, 15, 15);
-    const earthMaterial = new THREE.MeshPhongMaterial({map: loader.load("image/earth.jpeg"),
-                                                      bumpMap: loader.load('image/elev_bump_8k.jpg')})
+    const earthMaterial = new THREE.MeshPhongMaterial({
+        map: loader.load("image/earth.jpeg"),
+        bumpMap: loader.load('image/elev_bump_8k.jpg')
+    })
     const earthMesh = new THREE.Mesh(earthGeo, earthMaterial);
     earthMesh.position.set(50, 0, 0)
     scene.add(earthMesh)
     objects.push(earthMesh)
 
-    // animation
-    let start;
+
     function render(timestamp) {
-        if (start === undefined)
-            start = timestamp;
-        const elapsed = timestamp - start;
+        if (start === undefined) {
+            start = currentTime;
+        }
+        if (state) {
+            currentTime = timestamp
+        }
+        // for debug, suggest to delete at the final phase
+        console.log("state ", state, "currentTime: ", currentTime, " pauseTime: ", pauseTime)
+
+        pauseTime = currentTime
+        const elapsed = (currentTime - start)/speed;
+        console.log("timestamp: ",currentTime," start: ", start)
         objects.forEach((obj) => {
-            obj.rotation.y = elapsed * 0.001;
+                    obj.rotation.y = elapsed * 0.001;
         })
+
         rotateEarth(earthMesh, elapsed * 0.01)
         renderer.render(scene, camera);
         requestAnimationFrame(render);
+        }
 
-    }
-    requestAnimationFrame(render);
 }
 
 // rotate the Earth around the Sun
@@ -81,6 +125,8 @@ function rotateEarth(obj, time) {
     obj.position.x = Math.cos(time * 0.1 + 5) * 50;
     obj.position.z = Math.sin(time * 0.1 + 5) * 50;
 }
+
+
 
 main();
 
