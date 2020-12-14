64c1bcb21f893646dae2b012caa30ba0eab9ff30
fangjixu <mindyxu776@gmail.com>
2020-12-14 22:25:32 +0800

    implement server

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.listen(3000);
 
+var http = require('http')
+var server = http.Server(app)
+
+app.get("/", (req, res) => {
+    res.sendFile('public/index.html', {root:__dirname})
+})
+
+app.listen(PORT, function () {
+    console.log("server is running")
+});
